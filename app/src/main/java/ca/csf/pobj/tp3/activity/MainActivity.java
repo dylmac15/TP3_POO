ff776bcf20f199ad0939078ad96ae3b4d2686bca
Dylan <dylan.maclellan@hotmail.com>
2018-12-05 15:42:26 -0500

    first commit

---
+++ app/src/main/java/ca/csf/pobj/tp3/activity/MainActivity.java
@@ -0,0 +1,84 @@
+package ca.csf.pobj.tp3.activity;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Random;
+
+import ca.csf.pobj.tp3.R;
+import ca.csf.pobj.tp3.utils.view.CharactersFilter;
+import ca.csf.pobj.tp3.utils.view.KeyPickerDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int KEY_LENGTH = 5;
+    private static final int MAX_KEY_VALUE = (int) Math.pow(10, KEY_LENGTH) - 1;
+
+    private View rootView;
+    private EditText inputEditText;
+    private TextView outputTextView;
+    private TextView currentKeyTextView;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rootView = findViewById(R.id.rootView);
+        progressBar = findViewById(R.id.progressbar);
+        inputEditText = findViewById(R.id.input_edittext);
+        inputEditText.setFilters(new InputFilter[]{new CharactersFilter()});
+        outputTextView = findViewById(R.id.output_textview);
+        currentKeyTextView = findViewById(R.id.current_key_textview);
+    }
+
+    private void showKeyPickerDialog(int key) {
+        KeyPickerDialog.make(this, KEY_LENGTH)
+                .setKey(key)
+                .setConfirmAction(this::fetchSubstitutionCypherKey)
+                .show();
+    }
+
+    private void showCopiedToClipboardMessage() {
+        Snackbar.make(rootView, R.string.text_copied_output, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void showConnectionError() {
+        Snackbar.make(rootView, R.string.text_connectivity_error, Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.text_activate_wifi, (view) -> showWifiSettings())
+                .show();
+    }
+
+    private void showServerError() {
+        Snackbar.make(rootView, R.string.text_server_error, Snackbar.LENGTH_INDEFINITE)
+                .show();
+    }
+
+    private void showWifiSettings() {
+        Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+        startActivity(intent);
+    }
+
+    private void fetchSubstitutionCypherKey(int key) {
+        //TODO
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private void putTextInClipboard(String text) {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        clipboard.setPrimaryClip(ClipData.newPlainText(getResources().getString(R.string.clipboard_encrypted_text), text));
+    }
+
+}
